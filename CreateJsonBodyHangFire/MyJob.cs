a21639c27a57dccea4f14917b37ac2e3ed44c17e
Aji Suryawan <kazumovie132@gmail.com>
2024-02-24 10:50:24 +0700

    Add project files.

---
+++ CreateJsonBodyHangFire/MyJob.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using Newtonsoft.Json;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateJsonBodyHangFire
+{
+    internal class MyJob : IJob
+    {
+
+        public void mainExecute()
+        {
+            string ReadFolder = ConfigurationManager.AppSettings["pathFolderRead"];
+            string pathFolderDestination = ConfigurationManager.AppSettings["pathFolderDestination"];
+            string fileNamePrefix = ConfigurationManager.AppSettings["fileNamePrefix"];
+            string latestCsvFile = GetLatestCsvFile(ReadFolder);
+
+            if (!string.IsNullOrEmpty(latestCsvFile))
+            {
+                try
+                {
+                    // Read the CSV file using StreamReader
+                    using (StreamReader reader = new StreamReader(latestCsvFile))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            string line = reader.ReadLine();
+                            // Process each line of the CSV file
+                            string sessionId = Guid.NewGuid().ToString(); // Generate a unique SessionId
+                            string company = "CE31"; // Replace with your actual company value
+
+                            // Form JSON object
+                            string jsonData = JsonConvert.SerializeObject(new
+                            {
+                                SessionId = sessionId,
+                                Company = company,
+                                Data = line
+                            });
+
+                            // Print or process the generated JSON string
+                            Console.WriteLine(jsonData);
+                            // Write the JSON data to a file in the destination folder with a unique name
+                            string fileName = $"{fileNamePrefix}_{sessionId}.json";
+                            string fullPath = Path.Combine(pathFolderDestination, fileName);
+                            File.WriteAllText(fullPath, jsonData);
+                            Console.WriteLine($"JSON data written to file: {fullPath}");
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error reading CSV file: " + ex.Message);
+                }
+            }
+
+        }
+
+        private string GetLatestCsvFile(string folderPath)
+        {
+            // Get the latest CSV file in the folder based on creation time
+            try
+            {
+                var csvFiles = Directory.GetFiles(folderPath, "*.csv");
+                if (csvFiles.Length > 0)
+                {
+                    return csvFiles.OrderByDescending(f => new FileInfo(f).CreationTime).First();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error getting latest CSV file: " + ex.Message);
+            }
+
+            return null;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            mainExecute();
+        }
+    }
+}
---
+++ CreateJsonBodyHangFire/Service1.cs
@@ -0,0 +1,86 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateJsonBodyHangFire
+{
+    public partial class Service1 : ServiceBase
+    {
+        private IScheduler _scheduler;
+
+        public void onDEBUG()
+        {
+            MyJob job = new MyJob();
+            job.mainExecute();
+        }
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string CronExpression = ConfigurationManager.AppSettings["CronExpression"];
+            string TimeZone_ = ConfigurationManager.AppSettings["TimeZone_"];
+            string windowsZoneId = TZConvert.RailsToWindows(TimeZone_);
+            TimeZoneInfo timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(windowsZoneId);
+
+            // Configure the Quartz scheduler
+            ISchedulerFactory schedulerFactory = new StdSchedulerFactory();
+            _scheduler = schedulerFactory.GetScheduler().Result;
+
+            string id = Guid.NewGuid().ToString();
+
+            IJobDetail job = JobBuilder.Create<MyJob>()
+                .WithIdentity("J" + id, "JG" + id)
+                .Build();
+
+            // Create and configure the trigger to run every day at 11 PM
+            /*ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("MyTrigger", "MyGroup")
+                .StartAt(DateBuilder.TodayAt(23, 0, 0))  // 11 PM
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInHours(24)  // Every 24 hours
+                    .RepeatForever())
+                .Build();*/
+
+            ITrigger trigger = null;
+            trigger = TriggerBuilder.Create()
+                    .WithIdentity("T" + id, "TG" + id)
+                    .StartNow()
+                    .WithCronSchedule(CronExpression, x => x.InTimeZone(timeZoneInfo))
+                    //.WithCronSchedule(CronExpression)
+                    .Build();
+
+            /*ITrigger trigger = TriggerBuilder.Create()
+            .WithIdentity("MyTrigger", "MyGroup")
+            .StartAt(DateBuilder.EvenMinuteDate(DateTimeOffset.UtcNow.AddMinutes(1)))  // Run on the next minute
+            .WithSimpleSchedule(x => x
+                .WithIntervalInMinutes(1)  // Every 1 minute
+                .RepeatForever())
+            .Build();*/
+
+            // Schedule the job with the trigger
+            _scheduler.ScheduleJob(job, trigger);
+
+            // Start the scheduler
+            _scheduler.Start();
+        }
+
+        protected override void OnStop()
+        {
+            // Shutdown the scheduler when the service is stopped
+            _scheduler?.Shutdown();
+        }
+    }
+}
